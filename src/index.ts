f0a7de108742516da841e4045d8c8fdc94793c2e
ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>
2025-09-30 06:25:17 +0530

    initial message handlers
    Signed-off-by: ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
-import { server as WebSocketServer } from "websocket";
+import { connection, server as WebSocketServer } from "websocket";
 import http from "http";
+import {
+  SupportedMessage as OutgoingSupportingMessage,
+  type OutgoingMessage,
+} from "./messages/outgoingMessages.js";
+import {
+  SupportedMessage,
+  type IncomingMessage,
+} from "./messages/incomingMessages.js";
+import { UserManager } from "./userManager.js";
+import { InMemoryStore } from "./store/inMemoryStore.js";
 
-var server = http.createServer(function (request: any, response: any) {
+const server = http.createServer(function (request: any, response: any) {
   console.log(new Date() + " Received request for " + request.url);
   response.writeHead(404);
   response.end();
 });
+
+const userManager = new UserManager();
+const store = new InMemoryStore();
 
 server.listen(8080, function () {
   console.log(new Date() + " Server is listening on port 8080");
@@ -13,16 +26,10 @@ server.listen(8080, function () {
 
 const wsServer = new WebSocketServer({
   httpServer: server,
-  // You should not use autoAcceptConnections for production
-  // applications, as it defeats all standard cross-origin protection
-  // facilities built into the protocol and the browser.  You should
-  // *always* verify the connection's origin and decide whether or not
-  // to accept it.
   autoAcceptConnections: false,
 });
 
 function originIsAllowed(origin: string) {
-  // put logic here to detect whether the specified origin is allowed.
   return true;
 }
 
@@ -39,14 +46,13 @@ wsServer.on("request", function (request) {
   var connection = request.accept("echo-protocol", request.origin);
   console.log(new Date() + " Connection accepted.");
   connection.on("message", function (message) {
+    //Todo: add rate limiting logic here
     if (message.type === "utf8") {
+      try {
+        messageHandler(connection, JSON.parse(message.utf8Data));
+      } catch (e) {}
       console.log("Received Message: " + message.utf8Data);
       connection.sendUTF(message.utf8Data);
-    } else if (message.type === "binary") {
-      console.log(
-        "Received Binary Message of " + message.binaryData.length + " bytes"
-      );
-      connection.sendBytes(message.binaryData);
     }
   });
   connection.on("close", function (reasonCode, description) {
@@ -55,3 +61,62 @@ wsServer.on("request", function (request) {
     );
   });
 });
+
+//type => JOIN_ROOM, message => InitMessage
+//type => SEND_MESSAGE, message => UserMessage
+//type =>  UPVOTE_MESSAGE, message => UpvoteMessage
+function messageHandler(ws: connection, message: IncomingMessage) {
+  if (message.type == SupportedMessage.JoinRoom) {
+    const payload = message.payload;
+    userManager.addUser(payload.name, payload.userId, payload.roomId, ws);
+  }
+  if (message.type == SupportedMessage.SendMessage) {
+    const payload = message.payload;
+    const user = userManager.getUser(payload.userId, payload.userId);
+
+    if (!user) {
+      console.error("User not found in the db");
+      return;
+    }
+
+    let chat = store.addChat(
+      payload.userId,
+      user?.name,
+      payload.roomId,
+      payload.message
+    );
+    if (!chat) {
+      return;
+    }
+    //Todo: broadcast this message to all users in the room
+    const outgoingPayload: OutgoingMessage = {
+      type: OutgoingSupportingMessage.AddChat,
+      payload: {
+        chatId: chat.id,
+        roomId: payload.roomId,
+        message: payload.message,
+        name: user.name,
+        upvotes: 0,
+      },
+    };
+    userManager.broadcast(payload.roomId, payload.userId, outgoingPayload);
+  }
+  if (message.type == SupportedMessage.UpvoteMessage) {
+    const payload = message.payload;
+    const chat = store.upvote(payload.userId, payload.roomId, payload.chatId);
+    if (!chat) {
+      return;
+    }
+
+    //Todo: broadcast this message to all users in the room
+    const outgoingPayload: OutgoingMessage = {
+      type: OutgoingSupportingMessage.UpdateChat,
+      payload: {
+        chatId: payload.chatId,
+        roomId: payload.roomId,
+        upvotes: chat.upvotes.length,
+      },
+    };
+    userManager.broadcast(payload.roomId, payload.userId, outgoingPayload);
+  }
+}
---
+++ src/userManager.ts
@@ -0,0 +1,56 @@
+import type { connection } from "websocket";
+import type { OutgoingMessage } from "./messages/outgoingMessages.js";
+
+interface User {
+  name: string;
+  id: string;
+  connection: connection;
+}
+
+interface Room {
+  users: User[];
+}
+export class UserManager {
+  private rooms: Map<string, Room>;
+  constructor() {
+    this.rooms = new Map<string, Room>();
+  }
+
+  addUser(name: string, userId: string, roomId: string, socket: connection) {
+    if (!this.rooms.get(roomId)) {
+      this.rooms.set(roomId, { users: [] });
+    }
+    this.rooms
+      .get(roomId)
+      ?.users.push({ name, id: userId, connection: socket });
+  }
+
+  removeUser(userId: string, roomId: string) {
+    const users = this.rooms.get(roomId)?.users;
+    if (users) {
+      users.filter(({ id }) => id !== userId);
+    }
+  }
+
+  getUser(userId: string, roomId: string): User | null {
+    const user = this.rooms.get(roomId)?.users.find(({ id }) => id === userId);
+    return user ?? null;
+  }
+
+  broadcast(roomId: string, userId: string, message: OutgoingMessage) {
+    const user = this.getUser(userId, roomId);
+    if (!user) {
+      console.error("User not found in the db");
+      return;
+    }
+    const room = this.rooms.get(roomId);
+    if (!room) {
+      console.error("Room not found");
+      return;
+    }
+
+    room.users.forEach(({ connection }) => {
+      connection.sendUTF(message);
+    });
+  }
+}
